79818fa2fbc2f8208227d83d80f02e4f94e09fde
taichi-ue <a.willow.taichi@gmail.com>
2026-06-18 00:40:53 +0900

    html

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'study-app-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/quick.html',
+  '/css/style.css',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/js/db.js',
+  '/js/notify.js',
+  '/js/home.js',
+  '/js/calendar.js',
+  '/js/stats.js',
+  '/js/categories.js',
+  '/js/settings.js',
+  '/js/streak.js',
+  '/js/app.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      }).catch(() => {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
